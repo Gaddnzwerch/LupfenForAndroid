8d8da27b9558ced80e3437cfef730820a94ae34c
Andreas Schultz <develop@himmel-hof.de>
2015-04-26 22:56:35 +0200

    first elements to choose application type (play/host/host+play)

--- app/src/main/java/iteem/lupfenforandroid/MainActivity.java
+++ app/src/main/java/iteem/lupfenforandroid/MainActivity.java
@@ -2,16 +2,31 @@ package iteem.lupfenforandroid;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button joinButton;
+    Button hostAndPlayButton;
+    Button hostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // link the buttons to the corresponding actions
+        joinButton = (Button) findViewById(R.id.join_button);
+        joinButton.setOnClickListener(this);
+        hostAndPlayButton = (Button) findViewById(R.id.host_play_button);
+        hostAndPlayButton.setOnClickListener(this);
+        hostButton = (Button) findViewById(R.id.host_button);
+        hostButton.setOnClickListener(this);
     }
 
 
@@ -36,5 +51,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==findViewById(R.id.join_button)) {
+            System.out.println("Join");
+        }
     }
 }
